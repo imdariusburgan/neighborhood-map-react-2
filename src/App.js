63db2b49174cbedb60c7e750c9085547b8003177
Darius Burgan <darius@dariusburgan.com>
2018-09-21 19:50:47 -0400

    Create function to load script tag to DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// This function will load the Google Maps API script in the DOM
+// This code was inspired from https://stackoverflow.com/questions/42847126/script-load-in-react
+function loadScriptUrl(url) {
+  let scriptTag = document.createElement("script");
+  scriptTag.src = url;
+  scriptTag.async = true;
+  scriptTag.defer = true;
+  document.body.appendChild(scriptTag);
+}
+
 class App extends Component {
   // This function initializes a new Google Maps
   initMap = () => {
-    map = new google.maps.Map(document.getElementById("map"), {
+    let map = new window.google.maps.Map(document.getElementById("map"), {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8
     });
