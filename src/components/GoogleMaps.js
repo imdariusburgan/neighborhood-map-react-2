cc4c3906be7f67f91270f50ac51aae45af753f92
Darius Burgan <darius@dariusburgan.com>
2018-09-27 23:13:59 -0400

    Initial commit

---
+++ src/components/GoogleMaps.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class GoogleMaps extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.markers !== this.props.markers) {
+      nextProps.markers.map(marker => {
+        marker.setMap(nextProps.map);
+        return null;
+      });
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="col-xs-12 col-md-8 order-md-12" id="map" />
+      </React.Fragment>
+    );
+  }
+}
